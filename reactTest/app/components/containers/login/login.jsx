d1562be60c409651e036618d005ab26ae39a8420
xiaochen111 <chb0413@163.com>
2017-07-22 14:48:49 +0800

    代码更新

--- reactTest/app/components/containers/login/login.jsx
+++ reactTest/app/components/containers/login/login.jsx
@@ -8,8 +8,24 @@ class Login extends Component{
 	render(){
 		return (
 			<div className="container">
-				<h1><a href="#/bodyBox">登录页面</a></h1>
-				<h1><a href="#/Member" onClick={this.login}>登录</a></h1>
+				<div className="login">
+					<h3 className="tit_login">登录</h3>
+					<div className="ipt_lab">
+						<label>用户名：</label>
+						<input type="text" placeholder="请输入用户名"/>
+					</div>
+					<div className="ipt_lab">
+						<label>密码：</label>
+						<input type="password" placeholder="请输入密码"/>
+					</div>
+					<div className="btn_login">
+						<a href="#/Member" onClick={this.login}>登录</a>
+					</div>
+					<div className="btns_tt">
+						<a className="fl">忘记密码</a>
+						<a href="#/bodyBox" className="fr">返回首页</a>
+					</div>
+				</div>
 			</div>
 		)
 	}
